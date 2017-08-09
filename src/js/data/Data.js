747b508fe8fced56112823810448a679a92834e1
AleksandrSKR <Skrebec2009@gmail.com>
2017-08-09 15:18:49 +0300

    first commit

---
+++ src/js/data/Data.js
@@ -0,0 +1,54 @@
+const data = {
+    "currencies": [
+        {
+            "code": "UAH",
+            "sellRate": 1,
+            "name": "Ukrainian Grivnas",
+            "sign": "₴"
+        },
+        {
+            "code": "RUB",
+            "sellRate": 2.27,
+            "name": "Russian RUB",
+            "sign": "₽"
+        },
+        {
+            "code": "USD",
+            "sellRate": .0389,
+            "name": "United States Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "AUD",
+            "sellRate": .0489,
+            "name": "Australian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "CAD",
+            "sellRate": .0492,
+            "name": "Canadian Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "NZD",
+            "sellRate": .0527,
+            "name": "New Zealand Dollars",
+            "sign": "$"
+        },
+        {
+            "code": "JPY",
+            "sellRate": 4.2865,
+            "name": "Japanese Yen",
+            "sign": "¥"
+        },
+        {
+            "code": "GBP",
+            "sellRate": .0298,
+            "name": "British Pounds Sterling",
+            "sign": "£"
+        }
+    ]
+}
+
+export default data;
